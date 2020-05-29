3e6ad3887649ffa73ba57d8abd4c8bcc9d9e18fe
turbo <turbo1992@163.com>
2020-05-29 17:20:46 +0800

    hide animation

--- YouTube/Class/ThirdParty/ESTabBar/Content/BouncesContentView.swift
+++ YouTube/Class/ThirdParty/ESTabBar/Content/BouncesContentView.swift
@@ -31,10 +31,10 @@ class BouncesContentView: BasicContentView {
     }
     
     func bounceAnimation() {
-        let impliesAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        impliesAnimation.values = [1.0 ,1.4, 0.9, 1.15, 0.95, 1.02, 1.0]
-        impliesAnimation.duration = duration * 2
-        impliesAnimation.calculationMode = CAAnimationCalculationMode.cubic
-        imageView.layer.add(impliesAnimation, forKey: nil)
+//        let impliesAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+//        impliesAnimation.values = [1.0 ,1.4, 0.9, 1.15, 0.95, 1.02, 1.0]
+//        impliesAnimation.duration = duration * 2
+//        impliesAnimation.calculationMode = CAAnimationCalculationMode.cubic
+//        imageView.layer.add(impliesAnimation, forKey: nil)
     }
 }
